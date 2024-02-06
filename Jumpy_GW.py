b3c49ff978dde825d2603d31d5c3b432db16b3bc
amirmat98 <matin.amirmahdi@gmail.com>
2024-02-06 15:34:34 +0100

    compleate Jumpy_GW

--- Jumpy_GW.py
+++ Jumpy_GW.py
@@ -24,4 +24,74 @@ class JumpyGW:
         return self.start
 
     # Verify the validity of a position in the grid        
+    def position_validation(self, position):
+        if ( (0 <= position[0] < self.grid_size) and (0 <= position[1] < self.grid_size) ):
+            return True
+        else:
+            return False
+    
+    # Determine whether a given position constitutes an obstacle
+    def obstacle_check(self, position):
+        if (position in self.obstacles):
+            return True
+        else:
+            return False
+    
+    # Determine whether a given position represents the goal state
+    def goal_check(self, position):
+        if (position == self.goal):
+            return True
+        else:
+            return False
 
+    # Action Effect Dictionary Definiation
+    Action_Effect = {
+        0: (-1, 0), # UP
+        1: (1, 0), # Down
+        2: (0, -1), # Left
+        3: (0, 1), #Right
+    }
+    
+    # Execute the designated action and provide the updated position and reward
+    def action_execuation(self, position, action):
+        if action in self.Action_Effect:
+            delta = self.Action_Effect[action]
+            new_positon = (position[0] + delta[0], position[1] + delta[1])
+        else:
+            jump_direction = np.random.choice([-1,1], size = 2)
+            new_positon = (position[0] + jump_direction[0], position[1] + jump_direction[1])
+
+        # update postion if valid and not an obstacle
+        new_positon = self.update_position(position, new_positon)
+
+        # update grid
+        self.update_grid(position, new_positon)
+
+        # calculate reward
+        reward = self.calculate_reward(new_positon)
+
+        # add the current position to the agent's path
+        self.agent_path.append(new_positon)
+
+        return new_positon, reward
+    
+
+    # define update position
+    def update_position(self, position, new_positon):
+        if self.position_validation(new_positon) and not self.obstacle_check(new_positon):
+            return new_positon
+        else:
+            return position
+    
+    # define update grid
+    def update_grid(self, position, new_positon):
+        self.grid[position] = 0
+        self.grid[new_positon] = 1
+    
+    # define calculation reward
+    def calculate_reward(self, position):
+        if self.goal_check(position):
+            return 10
+        else:
+            return -1
+    
